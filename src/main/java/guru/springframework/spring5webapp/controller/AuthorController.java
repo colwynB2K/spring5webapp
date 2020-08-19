8cd6112038633fa18789a8a95f6a1b0d005104bc
Ivo Eersels <ivo.eersels@gmail.com>
2020-08-19 15:08:22 +0200

    - Add MVC Controller for Authors - Add a author list Thymeleaf template

---
+++ src/main/java/guru/springframework/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,29 @@
+package guru.springframework.spring5webapp.controller;
+
+import guru.springframework.spring5webapp.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/authors")
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @GetMapping("/list")
+    public String getAuthors(Model model) {
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+
+}
